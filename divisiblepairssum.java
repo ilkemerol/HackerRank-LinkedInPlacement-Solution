7cd9da9789baff6ed4afeae4ba02a19e4cfbfffe
İlkem Erol <ilkem.erol@hotmail.com>
2017-02-25 22:27:31 +0200

    Create divisiblepairssum.java

---
+++ divisiblepairssum.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int global_result = 0;
+        for(int i = 0; i < n-1; i++){
+            for(int j = i+1; j < n; j++){
+                int sum = a[i] + a[j];
+                int result = sum % k;
+                if(result == 0){
+                    global_result++;
+                }
+            }
+        }
+        System.out.println(global_result);
+    }
+}
