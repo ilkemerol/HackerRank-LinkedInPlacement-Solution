ee131b5e1020faff78f327a87542e193d43cbfdf
İlkem Erol <ilkem.erol@hotmail.com>
2017-03-07 08:37:21 +0200

    Create dictionariesandmaps.java

---
+++ dictionariesandmaps.java
@@ -0,0 +1,30 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap <String, Integer> hm = new HashMap <String, Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            //System.out.println(name + " " +phone);
+            hm.put(name, phone);
+        }
+        Set <String> allKey = hm.keySet();
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(hm.containsKey(s)){
+                System.out.println(s + "=" + hm.get(s));
+            }
+            else{
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
